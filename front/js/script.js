30f09367af44d279f1553b30e15be28fd80005d4
A-p-p-l-a <paulmahot33@gmail.com>
2022-06-28 04:08:29 +0200

    add fetch

---
+++ front/js/script.js
@@ -0,0 +1,12 @@
+fetch(“.../back”)
+  .then(function(res) {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  .then(function(value) {
+    console.log(value);
+  })
+  .catch(function(err) {
+    // Une erreur est survenue
+  });
